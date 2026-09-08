156e68ecc6f0dca1d014290a49686127d4f01e04
Ziad Tamer <225040@eru.edu.eg>
2026-09-08 21:33:50 +0300

    Create Splash View Body

--- lib/features/splash/presentation/view/splash_view.dart
+++ lib/features/splash/presentation/view/splash_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:yum_quick/features/splash/presentation/view/widgets/splash_view_body.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(body: const SplashViewBody());
   }
 }
