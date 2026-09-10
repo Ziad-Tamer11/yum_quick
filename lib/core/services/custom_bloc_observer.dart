5f8ec146d4d02e251dc53ff90994da7847c4bafd
Ziad Tamer <225040@eru.edu.eg>
2026-09-10 06:31:10 +0300

    feat: Add CustomBlocObserver for enhanced Bloc state management and error logging

---
+++ lib/core/services/custom_bloc_observer.dart
@@ -0,0 +1,17 @@
+import 'dart:developer';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CustomBlocObserver extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    log('onChange in ${bloc.runtimeType}: $change');
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    log('onError in ${bloc.runtimeType}: $error');
+    super.onError(bloc, error, stackTrace);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:yum_quick/core/di/service_locator.dart';
+import 'package:yum_quick/core/services/custom_bloc_observer.dart';
 import 'package:yum_quick/core/services/shared_preferences_singleton.dart';
 import 'package:yum_quick/core/utils/app_router.dart';
 import 'package:yum_quick/firebase_options.dart';
@@ -11,6 +13,7 @@ void main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await Prefs.init();
   setupServiceLocator();
+  Bloc.observer = CustomBlocObserver();
   runApp(const YumQuick());
 }
 
