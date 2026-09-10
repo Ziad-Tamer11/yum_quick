461d4e8111072bd1eae51183c73d6b10bbdd88c2
Ziad Tamer <225040@eru.edu.eg>
2026-09-10 04:03:36 +0300

    feat: Add Firebase plugins and UserEntity model for authentication

---
+++ lib/features/auth/domain/entities/user_entity.dart
@@ -0,0 +1,15 @@
+class UserEntity {
+  final String uid;
+  final String name;
+  final String email;
+  final String phone;
+  final DateTime dateOfBirth;
+
+  const UserEntity({
+    required this.uid,
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.dateOfBirth,
+  });
+}
