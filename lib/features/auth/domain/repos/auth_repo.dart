4e8851b3a16fbd7bb8f410e95aa01e44fe4fa234
Ziad Tamer <225040@eru.edu.eg>
2026-09-10 04:16:14 +0300

    Create Auth Repo

---
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+import 'package:yum_quick/core/errors/failure.dart';
+import 'package:yum_quick/features/auth/domain/entities/user_entity.dart';
+
+abstract class AuthRepo {
+  Future<Either<Failure, UserEntity>> login({
+    required String email,
+    required String password,
+  });
+
+  Future<Either<Failure, UserEntity>> signUp({
+    required String name,
+    required String email,
+    required String password,
+    required String phone,
+    required DateTime dateOfBirth,
+  });
+}
