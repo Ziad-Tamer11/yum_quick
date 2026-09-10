0f9eff07566c813805ff003df3a9d1bb5967ef27
Ziad Tamer <225040@eru.edu.eg>
2026-09-10 06:43:56 +0300

    feat: Refactor LoginView to use BlocProvider and implement LoginViewBodyBlocConsumer for state management

--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:yum_quick/core/di/service_locator.dart';
 import 'package:yum_quick/core/utils/app_colors.dart';
-import 'package:yum_quick/features/auth/presentation/views/widgets/login_view_body.dart';
+import 'package:yum_quick/features/auth/domain/usecases/auth_usecase.dart';
+import 'package:yum_quick/features/auth/presentation/manager/login_cubit/login_cubit.dart';
+import 'package:yum_quick/features/auth/presentation/views/widgets/login_view_body_bloc_consumer.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
@@ -9,7 +13,13 @@ class LoginView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.yellowBase,
-      body: SafeArea(bottom: false, child: LoginViewBody()),
+      body: SafeArea(
+        bottom: false,
+        child: BlocProvider(
+          create: (context) => LoginCubit(authUseCase: getIt<AuthUseCase>()),
+          child: LoginViewBodyBlocConsumer(),
+        ),
+      ),
     );
   }
 }
---
+++ lib/features/auth/presentation/views/widgets/login_view_body_bloc_consumer.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:yum_quick/core/utils/app_colors.dart';
+import 'package:yum_quick/core/widgets/show_message_bar.dart';
+import 'package:yum_quick/features/auth/presentation/manager/login_cubit/login_cubit.dart';
+import 'package:yum_quick/features/auth/presentation/views/widgets/login_view_body.dart';
+
+class LoginViewBodyBlocConsumer extends StatelessWidget {
+  const LoginViewBodyBlocConsumer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<LoginCubit, LoginState>(
+      listener: (context, state) {
+        if (state is LoginSuccess) {
+          showMessageBar(context, 'Login Success', Colors.green);
+        }
+        if (state is LoginFailure) {
+          showMessageBar(context, state.errMessage, AppColors.red);
+        }
+      },
+      builder: (context, state) {
+        return ModalProgressHUD(
+          inAsyncCall: state is LoginLoading ? true : false,
+          child: const LoginViewBody(),
+        );
+      },
+    );
+  }
+}
