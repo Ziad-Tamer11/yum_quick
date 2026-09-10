36a2efe74d43ab8e7735e9024c488a21b36c8d4d
Ziad Tamer <225040@eru.edu.eg>
2026-09-10 21:57:26 +0300

    Create Custom Divider

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:yum_quick/constants.dart';
 import 'package:yum_quick/core/widgets/custom_body_container.dart';
+import 'package:yum_quick/core/widgets/custom_divider.dart';
 import 'package:yum_quick/features/home/presentation/views/widgets/category_list.dart';
 import 'package:yum_quick/features/home/presentation/views/widgets/custom_home_app_bar.dart';
 
@@ -37,7 +38,7 @@ class _HomeViewBodyState extends State<HomeViewBody> {
                 right: kHorizontalPadding,
                 top: 31,
               ),
-              child: Column(children: [CategoryList()]),
+              child: Column(children: [CategoryList(), CustomDivider()]),
             ),
           ),
         ),
